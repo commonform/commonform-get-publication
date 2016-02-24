14efbe2b5f45b3fe8b96afec0e327d19d2d34b8d
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-24 00:44:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+```javascript
+var getProject = require('commonform-get-project')
+var assert = require('assert')
+
+getProject('ironsides', 'stockpower', '1e4d', function(error, project) {
+  assert.equal(
+    project.form,
+    '6b9d9e9b13b36ae00feb26abbd292b11d260c6f524cbd2795cc4445819580a64') })
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+/* Copyright 2016 Kyle E. Mitchell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module.exports = getProject
+
+var https = require('https')
+var parse = require('json-parse-errback')
+
+function getProject(publisher, project, edition, callback) {
+  https.request(
+    { host: 'projects.commonform.org',
+      path:
+        ( '/publishers/' + publisher +
+          '/projects/' + project +
+          '/editions/' + edition ) },
+    function(response) {
+      var buffers = [ ]
+      response
+        .on('data', function(buffer) {
+          buffers.push(buffer) })
+        .on('end', function() {
+          var responseBody = Buffer.concat(buffers).toString()
+          parse(responseBody, function(error, project) {
+            if (error) {
+              callback(error) }
+            else {
+              callback(null, project) } }) }) })
+    .end() }
